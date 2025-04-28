8bcf6d9c2a2525e7477eab67ac224a3f8f15076d
reanru <runtufandi@gmail.com>
2025-04-28 19:58:50 +0800

    create simple api routes

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
 
 export default function Index() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/product")
+    .then(res => res.json())
+    .then(res => {
+      console.log('res ', res.data);
+      setProducts(res.data);
+    })
+  }, [])
+  
+
   return (
     <div>
         <h1>Product Page</h1>
+        {products.map((data: productType) => (
+          <div key={data.id}>{data.name}</div>
+        ))}
     </div>
   )
 }
